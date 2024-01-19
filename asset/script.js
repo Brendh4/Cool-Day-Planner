5762d6aa3305d5001154ff6b06a1b91d670cb38a
Brendh4 <brendhauchiha@gmail.com>
2024-01-19 17:24:13 +0000

    adds class code

---
+++ asset/script.js
@@ -0,0 +1,23 @@
+console.log("this is")
+// todays date, put att top of page
+
+var startHour = "9 am"
+var endHour = "5 pm"
+
+
+// make a block for every hour from 9 to 5 inclusive
+
+// each block should allow for text entry (input element)
+    // each block should be styled if the hour is past pres or future - based on hour of day
+
+//each block should include an hour label
+    // associate each hours tasks with a separate key
+// each block should include a save button
+    // eachsave button should have a clikck handler that writes to the local storage
+
+var myDiv = document.createElement("div");
+myDiv.textContent = "Here is my first task"
+
+// save text to local storage - event handling
+
+// each block should load stored tasks for the given hour
