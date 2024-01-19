25f33b7b244ad5afff62e015e3d5533de7ccc5c7
Brendh4 <brendhauchiha@gmail.com>
2024-01-19 18:56:10 +0000

    adds save button button to timeblocks

--- asset/script.js
+++ asset/script.js
@@ -19,37 +19,24 @@ function generateTimeblocks () {
             timeblock.html('<div col-8 col-md-10>' + i + '</div>');
 
             timeblock.append(createInputField(i));
+            
+            var saveButton = $("<button>");
+            saveButton.addClass("saveBtn");
+            saveButton.html('<i class="fas fa-save"></i>');
+            timeblock.append(saveButton);
+
             timeBlocksEl.append(timeblock);
         }
     }
     
-    function createInputField(i) {
-        var inputField = $("<input>");
-        inputField.attr("type", "text");
-        inputField.attr("id", "inputField" + i);
-        inputField.addClass("col-8 col-md-10 description");
-        return inputField;
-    }
+function createInputField(i) {
+    var inputField = $("<input>");
+    inputField.attr("type", "text");
+    inputField.attr("id", "inputField" + i);
+    inputField.addClass("description");
+    return inputField;
+}
+
     
     generateTimeblocks();
 
-
-
-
-
-
-
-// each block should allow for text entry (input element)
-    // each block should be styled if the hour is past pres or future - based on hour of day
-
-//each block should include an hour label
-    // associate each hours tasks with a separate key
-// each block should include a save button
-    // eachsave button should have a clikck handler that writes to the local storage
-
-// var myDiv = document.createElement("div");
-// myDiv.textContent = "Here is my first task"
-
-// save text to local storage - event handling
-
-// each block should load stored tasks for the given hour
